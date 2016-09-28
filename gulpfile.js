7d60bb8143afd5ffb584e69ff6bbb0ade410f8a6
redd <danielonodje@gmail.com>
2016-09-28 05:34:38 +0100

    add configure gulp-mocha for testing [#130341269]

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ var gulpSequence = require('run-sequence');
 var bower = require('gulp-bower');
 var filter = require('gulp-filter');
 var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var mocha = require('gulp-mocha');
 
 
 gulp.task('default',function(){
@@ -16,9 +19,24 @@ gulp.task('bower', function() {
     .pipe(gulp.dest('./public/lib'));
 });
 
-gulp.tast('lint',function(){
+gulp.task('lint',function(){
   var jsFilter = filter(['gruntfile.js', 'public/js/**/*.js', 'test/**/*.js', 'app/**/*.js']);
-  gulp.src('./**/*.js')
+  return gulp.src('./**/*.js')
   .pipe(jsFilter)
   .pipe(jshint);
+});
+
+gulp.task('sass',function(){
+  return gulp.src('public/css/common.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error',sass.logError))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('public/css/common.css'));
+});
+
+gulp.task('test',function(){
+  return gulp.src(['test/**/*.js'],{read: false})
+  .pipe(mocha({
+    reporter: 'spec'
+  }));
 });
