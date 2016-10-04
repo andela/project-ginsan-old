abe6409eaf825cd54c6c9661fca9b3680739612f
andela-akolawole <aladekolawole15@yahoo.com>
2016-10-04 15:03:12 +0100

    unstage bower_components [Fixes #130341275]

--- config/env/all.js
+++ config/env/all.js
@@ -1,9 +1,9 @@
 var path = require('path'),
 rootPath = path.normalize(__dirname + '/../..');
 var keys = rootPath + '/keys.txt';
-var localDb = 'mongodb://localhost:27017/cfh';
+var localDb = '';
 module.exports = {
 	root: rootPath,
 	port: process.env.PORT || 3000,
-    db: process.env.MONGOHQ_URL || localDb
+    db: process.env.DB
 };
